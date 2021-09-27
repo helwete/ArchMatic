f37b2fa0940a7fb6c4af7a6701899183cdb709e5
helwete <mikael.andreas.nordin@gmail.com>
2021-09-27 19:52:10 -0400

    Initial config

--- 1-base.sh
+++ 1-base.sh
@@ -25,7 +25,6 @@ PKGS=(
         'awesome'               # Awesome Desktop
         'xfce4-power-manager'   # Power Manager 
         'rofi'                  # Menu System
-        'picom'                 # Translucent Windows
         'xclip'                 # System Clipboard
         'gnome-polkit'          # Elevate Applications
         'lxappearance'          # Set System Themes
@@ -38,7 +37,6 @@ PKGS=(
         'wpa_supplicant'            # Key negotiation for WPA wireless networks
         'dialog'                    # Enables shell scripts to trigger dialog boxex
         'openvpn'                   # Open VPN support
-        'networkmanager-openvpn'    # Open VPN plugin for NM
         'network-manager-applet'    # System tray icon/utility for network connectivity
         'libsecret'                 # Library for storing passwords
     
@@ -49,20 +47,12 @@ PKGS=(
         'pulseaudio-alsa'   # ALSA configuration for pulse audio
         'pavucontrol'       # Pulse Audio volume control
         'pnmixer'           # System tray volume control
-
-    # --- Bluetooth
-        'bluez'                 # Daemons for the bluetooth protocol stack
-        'bluez-utils'           # Bluetooth development and debugging utilities
-        'bluez-firmware'        # Firmwares for Broadcom BCM203x and STLC2300 Bluetooth chips
-        'blueberry'             # Bluetooth configuration tool
-        'pulseaudio-bluetooth'  # Bluetooth support for PulseAudio
     
     # --- Printers
         'cups'                  # Open source printer drivers
         'cups-pdf'              # PDF support for cups
         'ghostscript'           # PostScript interpreter
         'gsfonts'               # Adobe Postscript replacement fonts
-        'hplip'                 # HP Drivers
         'system-config-printer' # Printer setup  utility
 )
 
